341490d7dc5ff6d5c65a2287cdb68461bab9bcd6
clay53 <clayton.hickey@yahoo.com>
2019-01-19 22:45:36 -0500

    Created support for games and software

--- index.js
+++ index.js
@@ -1,8 +1,97 @@
-var gcloud = require('gcloud');
+var admin = require("firebase-admin");
 
-var storage = gcloud.storage({
-  projectId: 'claytondoesthingsxyz',
-  keyFilename: 'service-account-credentials.json'
+var serviceAccount = require("./serviceAccountKey.json");
+var app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'claytondoesthingsxyz.appspot.com'
 });
 
-var bucket = storage.bucket('claytondoesthingsxyz.appspot.com');
+var bucket = admin.storage().bucket();
+var db = admin.firestore();
+
+var calls = 0;
+var completedCalls = 0;
+
+var url = "https://claytondoesthings.xyz/";
+
+currentXML = (
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+  "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
+);
+
+// map root
+
+addUrl({
+  loc: url,
+  changeFreq: "monthly",
+  priority: 1
+});
+
+// map sub pages
+
+addUrl({
+  loc: (url + "games/"),
+  changeFreq: "weekly",
+  priority: "0.9"
+});
+
+// map games
+mapGameSoftware("games");
+
+// map softwares
+mapGameSoftware("softwares");
+
+function addUrl(options) {
+  currentXML += " <url>\n";
+  for (var option in options) {
+    currentXML += ("   <" + option + ">" + options[option] + "<" + option + ">\n");
+  }
+  currentXML += " </url>\n";
+}
+
+function checkFinishXML() {
+  completedCalls++;
+  console.log("calls: " + calls + " completedCalls: " + completedCalls);
+  if (calls === completedCalls) {
+    currentXML += "</urlset>";
+    console.log(currentXML);
+  }
+}
+
+function mapGameSoftware(theOne) {
+  calls++;
+  db.collection(theOne).get().then((gameQuery) => {
+    gameQuery.forEach((gameSnapshot) => {
+      addUrl({
+        loc: (url + theOne + "/" + gameSnapshot.id + "/"),
+        changeFreq: "weekly",
+        priority: "0.8"
+      });
+
+      calls++;
+      db.collection(theOne).doc(gameSnapshot.id).collection("platforms").get().then((platformQuery) => {
+        platformQuery.forEach((platform) => {
+          addUrl({
+            loc: (url + theOne + "/" + gameSnapshot.id + "/" + platform.data().name + "/"),
+            changeFreq: "weekly",
+            priority: "0.7"
+          });
+
+          calls++;
+          db.collection(theOne).doc(gameSnapshot.id).collection("platforms").doc(platform.id).collection("versions").get().then((versionQuery) => {
+            versionQuery.forEach((version) => {
+              addUrl({
+                loc: (url + theOne + "/" + gameSnapshot.id + "/" + platform.data().name + "/" + version.data().name),
+                changeFreq: "yearly",
+                priority: "0.6"
+              })
+            });
+            checkFinishXML();
+          });
+        });
+        checkFinishXML();
+      });
+    });
+    checkFinishXML();
+  });
+}
